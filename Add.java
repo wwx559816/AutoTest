09c654e38bc463936ffbf52b003af741ef5c9c5d
wwx559816 <43487804+wwx559816@users.noreply.github.com>
2018-10-27 12:38:40 +0800

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,5 +1,8 @@
 public class Add {
     public static void main(String[] args){
+        System.out.println("-------------------------------------------------");
         System.out.println("This is my first jenkins demo");
+        System.out.println("This is my first jenkins demo");
+        System.out.println("-------------------------------------------------");
     }
 }
